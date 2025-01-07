21f84c3eee712fbdb43dd0502149700b2db2a286
Marko <marko.falatec@ecodica.eu>
2025-01-07 14:11:42 +0100

    [2277;ODOO14;FIX]: Prevent data and table deletion on module uninstall

--- odoo/addons/base/models/ir_model.py
+++ odoo/addons/base/models/ir_model.py
@@ -267,7 +267,8 @@ class IrModel(models.Model):
                 if kind == 'v':
                     self._cr.execute(sql.SQL('DROP VIEW {}').format(sql.Identifier(table)))
                 elif kind == 'r':
-                    self._cr.execute(sql.SQL('DROP TABLE {} CASCADE').format(sql.Identifier(table)))
+                    _logger.warning('NOT dropping table: {} CASCADE'.format(sql.Identifier(table)))
+                    #self._cr.execute(sql.SQL('DROP TABLE {} CASCADE').format(sql.Identifier(table)))
                     # discard all translations for this model
                     self._cr.execute("""
                         DELETE FROM ir_translation
@@ -734,9 +735,11 @@ class IrModelFields(models.Model):
                 # TODO: Refactor this brol in master
                 if is_model and tools.column_exists(self._cr, model._table, field.name) and \
                         tools.table_kind(self._cr, model._table) == 'r':
-                    self._cr.execute(sql.SQL('ALTER TABLE {} DROP COLUMN {} CASCADE').format(
-                        sql.Identifier(model._table), sql.Identifier(field.name),
-                    ))
+                    _logger.warning('NOT cascade dropping column {}.{}'.format(
+                        sql.Identifier(model._table), sql.Identifier(field.name)))
+                    #self._cr.execute(sql.SQL('ALTER TABLE {} DROP COLUMN {} CASCADE').format(
+                    #    sql.Identifier(model._table), sql.Identifier(field.name),
+                    #))
                 if field.state == 'manual' and field.ttype == 'many2many':
                     rel_name = field.relation_table or (is_model and model._fields[field.name].relation)
                     tables_to_drop.add(rel_name)
@@ -756,7 +759,8 @@ class IrModelFields(models.Model):
                              (tuple(tables_to_drop), tuple(self.ids)))
             tables_to_keep = set(row[0] for row in self._cr.fetchall())
             for rel_name in tables_to_drop - tables_to_keep:
-                self._cr.execute(sql.SQL('DROP TABLE {}').format(sql.Identifier(rel_name)))
+                _logger.warning('Not dropping table {}.'.format(sql.Identifier(rel_name)))
+                #self._cr.execute(sql.SQL('DROP TABLE {}').format(sql.Identifier(rel_name)))
 
         return True
 
@@ -1665,8 +1669,9 @@ class IrModelRelation(models.Model):
 
         # drop m2m relation tables
         for table in to_drop:
-            self._cr.execute(sql.SQL('DROP TABLE {} CASCADE').format(sql.Identifier(table)))
-            _logger.info('Dropped table %s', table)
+            _logger.warning('NOT dropping table: {} '.format(sql.Identifier(table)))
+            #self._cr.execute(sql.SQL('DROP TABLE {} CASCADE').format(sql.Identifier(table)))
+            #_logger.info('Dropped table %s', table)
 
     def _reflect_relation(self, model, table, module):
         """ Reflect the table of a many2many field for the given model, to make
